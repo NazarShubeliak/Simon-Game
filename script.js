cb997c0241235da2898e1a32cede728295bc467c
Nazar Shubeliak <urgisweb@gmail.com>
2025-08-14 22:57:23 +0300

    add sound pick and button logic

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function randomChoseColor(index) {
 }
 
 function startGame() {
-  for (let i = 0; i < gamePattern.length; i++) {
-    $("#" + gamePattern[i].slice(1)).fadeOut(100).fadeIn(100);
-  }
+  let index = gamePattern.length - 1;
+  $("#" + gamePattern[index].slice(1)).fadeOut(100).fadeIn(100);
+  pickSoundEffect.play();
 }
 
 function nextSequence() {
@@ -15,10 +15,37 @@ function nextSequence() {
   startGame();
 }
 
-const buttonColors = ["#325d00", "#a31223", "#c9b40e", "#005cb8"];
-let gamePattern = [];
+function changeInfoTable(text) {
+  info.text(text);
+}
 
-nextSequence();
-// $(".red").fadeOut(100).fadeIn(100);
+const buttonColors = ["#325d00", "#a31223", "#c9b40e", "#005cb8"];
+const pickSoundEffect = new Audio("./pick.mp3");
+let gamePattern = [];
+let userChosenColour = [];
+let level = 0;
+let info = $(".game_info");
+let buttons = $(".game_button");
+let button_start = $(".button_start");
+
+buttons.on("click", (event) => {
+  pickSoundEffect.play();
+  userChosenColour.push("#" + event.target.id);
+  console.log(userChosenColour);
+  console.log(gamePattern);
+  // for (let i = 0; i < gamePattern.length; i++) {
+  //   if (gamePattern[i] !== userChosenColour[i]) {
+  //     changeInfoTable("Game Over");
+  //   }
+  // }
+  // nextSequence();
+  // gamePattern = [];
+});
+
+button_start.on("click", () => {
+  nextSequence();
+  button_start.fadeOut();
+  changeInfoTable("Start game " + level);
+})
 
 
